4a8fbf8cce5c756ed92f86f879f3c0eba4b95fe8
RafaelMRG <rafael.vozikis@hotmail.com>
2021-10-08 23:22:56 -0300

    Arrumação no código feito no vídeo de explicação de GIT

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import bibliotecas.biblio
+# Importei a biblioteca na pasta /bibliotecas/biblio.py nomeando a para lib
+import bibliotecas.biblio as lib
 
-print(biblioteca_print())
-print("Teste")
+# Guardei o return da função da biblioteca na variavel teste
+# Como a biblioteca foi renomeada para lib na hora de importar podemos chama-la com o prefixo lib. apenas
+teste = lib.biblioteca_print()
+
+
+# Printei o retorno da função da biblioteca
+print(teste)
