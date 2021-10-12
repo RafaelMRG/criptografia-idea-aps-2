88b371c46e6dd95bc9e143d856c3e453235e0317
RafaelMRG <rafael.vozikis@hotmail.com>
2021-10-12 17:38:23 -0300

    Sistema primitivo de input do usuário
    Sistema de input da senha do usuário primitivo que printa a confirmação de que a senha foi inserida corretamente mostrando a senha escondida com os caracteres "*", ex: A senha inserida foi *****    com a senha insiderida sendo Teste.

--- bibliotecas/biblio.py
+++ bibliotecas/biblio.py
@@ -1,2 +1,7 @@
 def biblioteca_print():
-    return "Return da biblioteca"
+    return "\n\nBiblioteca versão: 1.0\nRetorno completo\n\n"
+# os \n acima server para pular uma linha no terminal
+
+# Vamos usar a função abaixo para pedir o usuário para inserir sua senha
+def userInput(mensagem):
+    return str(input(mensagem))
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 # Importei a biblioteca na pasta /bibliotecas/biblio.py nomeando a para lib
-import bibliotecas.biblio as lib
-
-# Guardei o return da função da biblioteca na variavel teste
-# Como a biblioteca foi renomeada para lib na hora de importar podemos chama-la com o prefixo lib. apenas
-teste = lib.biblioteca_print()
+from bibliotecas.biblio import userInput as userInput, biblioteca_print as biblioversion
 
 
-# Printei o retorno da função da biblioteca
-print(teste)
+
+# Print da versão da biblioteca e confirmação que a biblioteca foi importada corretamente
+print(biblioversion())
+
+
+# Salvando a mensagem mostrada para o usuário na hora de pedir o input da senha
+msgInsiraSuaSenha = "Insira sua senha abaixo: \n"
+
+# Salvando a senha na variável senha com o input do usuário voltando como string (função da biblioteca)
+senha = userInput(msgInsiraSuaSenha)
+
+# Salvando o comprimento da senha para multiplicar o tamanho da senha pelo caracter * e "confirmar"	que a senha foi inserida
+comprimentoSenha = int(len(senha))
+
+# Confirmação que a senha foi inserida como explicado no comentário anterior
+print(f"\nSua senha é:{comprimentoSenha * '*'}\n")
